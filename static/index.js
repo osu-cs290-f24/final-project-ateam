37d890d7f6bc5c5633e090ff033fe15f90cfd6dd
Noah West <noahbwest5@gmail.com>
2024-12-06 12:55:39 -0800

    Added templates for js functions

---
+++ static/index.js
@@ -0,0 +1,75 @@
+// Make home button link to products page
+var homeButton = document.getElementById('home-start-button')
+if(homeButton) {
+    homeButton.onclick = function () {
+        location.href = "index.html";
+    };
+}
+
+// Store references to product page elements
+function clearReceipt()
+{
+
+
+
+
+}
+
+
+
+
+
+/*
+            <div class="reciept-items-container">
+                 <!-- receipt item-->
+                <div class="reciept-item" data-name="Water Bottle" data-amount="2" data-price="2">
+                    <span class="reciept-item-title">Water Bottle</a> <span class="reciept-item-amount">x2</span> <span class="reciept-item-price">$2</span>
+                </div>
+                 <!-- receipt item-->
+                <div class="reciept-item" data-name="Soda" data-amount="3" data-price="6">
+                    <span class="reciept-item-title">Soda</span> <span class="reciept-item-amount">x3</span> <span class="reciept-item-price">$6</span>
+                </div>
+            </div>
+            <div class="reciept-total" data-total="20">
+                <h3>Total - $20</h3>
+            </div>
+*/
+
+
+
+
+// Add itemType dropdown HTML
+// Completed transaction popup has okay button that returns to home page
+
+
+// Filtering products display by entered product name and itemType when user presses enter on search box
+
+
+
+
+
+// Cancel button should remove everything from receipt
+
+// Pay button should show payment popup
+
+
+// Add button on a product should add its information to the receipt and update the current receipt total
+
+
+// Payment popup cancel button should close payment popup
+
+
+// Payment popup cash/credit button should display popup for "Transaction completed"
+
+
+// Completed transaction popup okay button should return to home page
+
+
+/*
+        "name": "Beige Bed",
+        "price": "399.99",
+        "image": "Beds/Beige_Bed_F.avif",
+        "mouseImage": "Beds/Beige_Bed_S.avif",
+        "amount": "4",
+        "itemType": "Beds"
+*/
